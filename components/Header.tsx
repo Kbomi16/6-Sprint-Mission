8a787a062ad2c162eb2c77b0e9426a9f150eb803
김보미 <kimbomi2172@naver.com>
2024-06-04 19:31:17 +0900

    💄 ui: tailwind로 변경

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 import logo_text from '../public/assets/logo_text.png'
 import logo from '../public/assets/logo.png'
 import Image from 'next/image'
-import styles from '../styles/main.module.css'
 import Link from 'next/link'
 
 export default function Header() {
@@ -18,39 +17,46 @@ export default function Header() {
   const pathName = router.pathname
 
   return (
-    <div>
-      <div className={styles.header}>
-        <div onClick={goToMain}>
-          <div className={styles.logo}>
-            <picture>
-              <source
-                srcSet={logo_text.src}
-                media="(max-width: 768px)"
-                width={81}
-                height={40}
-              />
-              <Image src={logo} alt="로고" width={153} />
-            </picture>
-          </div>
-        </div>
-        <nav className={styles['header-nav']}>
-          <Link
-            href={'/boards'}
-            style={{ color: pathName === '/boards' ? '#3692FF' : 'inherit' }}
-          >
-            자유게시판
-          </Link>
-          <Link
-            href={'/items'}
-            style={{ color: pathName === '/items' ? '#3692FF' : 'inherit' }}
-          >
-            중고마켓
-          </Link>
-        </nav>
-        <button id="btn_small" onClick={goToSignin}>
-          로그인
-        </button>
+    <div className="fixed top-0 w-full flex justify-between items-center h-16 py-0 px-[24px] bg-white z-50 gap-4 lg:py-4 lg:px-48 lg:gap-12">
+      <div
+        onClick={goToMain}
+        className="cursor-pointer flex items-center justify-center"
+      >
+        <picture>
+          <source
+            srcSet={logo_text.src}
+            media="(max-width: 768px)"
+            width={81}
+            height={40}
+          />
+          <Image src={logo} alt="로고" width={153} />
+        </picture>
       </div>
+      <nav className="flex flex-row gap-2 flex-grow-3 text-lg font-bold md:gap-12">
+        <Link
+          href="/boards"
+          className={`hover:text-[--main] ${
+            pathName === '/boards' ? 'text-[--main]' : ''
+          }`}
+        >
+          자유게시판
+        </Link>
+        <Link
+          href="/items"
+          className={`hover:text-[--main] ${
+            pathName === '/items' ? 'text-[--main]' : ''
+          }`}
+        >
+          중고마켓
+        </Link>
+      </nav>
+      <button
+        id="btn_small"
+        onClick={goToSignin}
+        className="bg-[--btn1] text-white border-none cursor-pointer inline-flex items-center justify-center h-[42px] rounded-[0.5rem] px-[0.5rem] py-[1.5rem] w-[128px] hover:bg-[--btn2]"
+      >
+        로그인
+      </button>
     </div>
   )
 }
