dcee886acbc1615e2d82f17c9d312b7a63e55d93
김보미 <kimbomi2172@naver.com>
2024-06-05 12:17:44 +0900

    ✨ feat: 게시글 등록 페이지 구현

---
+++ pages/addPost.tsx
@@ -0,0 +1,90 @@
+import React, { KeyboardEvent, useState } from "react";
+import FileInput from "../components/FileInput";
+
+const INITIAL_VALUES = {
+  title: "",
+  content: "",
+  imgFile: null as File | null,
+};
+
+type AddPostProps = {
+  initialValues?: typeof INITIAL_VALUES;
+  initialPreview?: string;
+};
+
+function AddItem({
+  initialValues = INITIAL_VALUES,
+  initialPreview,
+}: AddPostProps) {
+  const [values, setValues] = useState<typeof INITIAL_VALUES>(initialValues);
+
+  const isDisabled = !values.title || !values.content;
+
+  const handleChange = (name: string, value: any) => {
+    setValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
+  };
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    const { name, value } = e.target;
+    handleChange(name, value);
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("title", values.title);
+    formData.append("content", values.content);
+    if (values.imgFile) {
+      formData.append("imgFile", values.imgFile);
+    }
+    setValues(INITIAL_VALUES);
+  };
+
+  return (
+    <div className="m-[8rem_auto_2rem] w-full max-w-[1200px] px-4 md:m-[0] md:p-[8rem_5rem_5rem] lg:m-[0_auto_5rem]">
+      <div className="mb-8 flex items-center justify-between">
+        <h3 className="text-xl font-semibold">게시글 쓰기</h3>
+        <button
+          className={`rounded-md px-4 py-2 ${isDisabled ? "cursor-not-allowed bg-gray-300" : "bg-blue-500 hover:bg-blue-600"} font-semibold text-white`}
+          disabled={isDisabled}
+          onClick={handleSubmit}
+        >
+          등록
+        </button>
+      </div>
+      <form onSubmit={handleSubmit} className="mt-4">
+        <h4 className="font-semibold">*제목</h4>
+        <input
+          type="text"
+          name="title"
+          value={values.title}
+          placeholder="제목을 입력해주세요"
+          onChange={handleInputChange}
+          className="my-4 mt-2 w-full rounded-md border-none bg-[--coolgray100] px-3 py-2 text-sm focus:outline-[--main]"
+        />
+        <h4 className="font-semibold">*내용</h4>
+        <textarea
+          name="content"
+          value={values.content}
+          placeholder="내용을 입력해주세요"
+          onChange={handleInputChange}
+          className="my-4 mt-2 w-full resize-none rounded-md border-none bg-[--coolgray100] px-3 py-2 text-sm focus:outline-[--main]"
+          rows={5}
+        />
+        <h4 className="font-semibold">이미지</h4>
+        <FileInput
+          name="imgFile"
+          value={values.imgFile}
+          initialPreview={initialPreview}
+          onChange={(name, file) => handleChange(name, file)}
+        />
+      </form>
+    </div>
+  );
+}
+
+export default AddItem;
