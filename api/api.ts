39eaba86119cb5f36bea61d4f7bbbc479793744c
김보미 <kimbomi2172@naver.com>
2024-06-05 10:42:10 +0900

    ♻️ refactor: api res 구조분해할당으로 변경

--- api/api.ts
+++ api/api.ts
@@ -2,13 +2,13 @@ import instance from "@/lib/axios";
 
 export async function getPosts({ orderBy = "", keyword = "" }) {
   const params = new URLSearchParams({ orderBy, keyword });
-  const response = await instance.get(`/articles?${params.toString()}`);
-  return response.data.list;
+  const { data } = await instance.get(`/articles?${params.toString()}`);
+  return data.list;
 }
 
 export async function getBestPosts({ pageSize = 3 }) {
-  const response = await instance.get(
+  const { data } = await instance.get(
     `/articles?&pageSize=${pageSize}&orderBy=like`,
   );
-  return response.data.list;
+  return data.list;
 }
