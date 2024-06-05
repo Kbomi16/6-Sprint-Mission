08fe6f2b66073a9643d643da1d39fc0472708b8d
김보미 <kimbomi2172@naver.com>
2024-06-05 10:38:40 +0900

    ♻️ refactor: URLSearchParams로 쿼리 수정

--- api/api.ts
+++ api/api.ts
@@ -1,14 +1,14 @@
-import instance from '@/lib/axios'
+import instance from "@/lib/axios";
 
-export async function getPosts({ orderBy = '', keyword = '' }) {
-  const query = `orderBy=${orderBy}&keyword=${keyword}`
-  const response = await instance.get(`/articles?${query}`)
-  return response.data.list
+export async function getPosts({ orderBy = "", keyword = "" }) {
+  const params = new URLSearchParams({ orderBy, keyword });
+  const response = await instance.get(`/articles?${params.toString()}`);
+  return response.data.list;
 }
 
 export async function getBestPosts({ pageSize = 3 }) {
   const response = await instance.get(
-    `/articles?&pageSize=${pageSize}&orderBy=like`
-  )
-  return response.data.list
+    `/articles?&pageSize=${pageSize}&orderBy=like`,
+  );
+  return response.data.list;
 }
