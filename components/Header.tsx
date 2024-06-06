d6dcadb5f4de1ca1892be04ab809c2986e4f4e9a
김보미 <kimbomi2172@naver.com>
2024-06-06 20:08:07 +0900

    💄 ui: 헤더 수정

--- components/Header.tsx
+++ components/Header.tsx
@@ -25,40 +25,42 @@ export default function Header() {
 
   return (
     <div className="fixed top-0 z-50 flex h-16 w-full items-center justify-between gap-4 bg-white px-[24px] py-0 lg:gap-12 lg:px-48 lg:py-4">
-      <div
-        onClick={goToMain}
-        className="flex cursor-pointer items-center justify-center"
-      >
-        <picture>
-          <source
-            srcSet={logo_text.src}
-            media="(max-width: 768px)"
-            width={81}
-            height={40}
-          />
-          <Image src={logo} alt="로고" width={153} />
-        </picture>
+      <div className="flex items-center gap-4">
+        <div
+          onClick={goToMain}
+          className="flex cursor-pointer items-center justify-center"
+        >
+          <picture>
+            <source
+              srcSet={logo_text.src}
+              media="(max-width: 768px)"
+              width={81}
+              height={40}
+            />
+            <Image src={logo} alt="로고" width={153} />
+          </picture>
+        </div>
+        <nav className="flex flex-row gap-2 text-lg font-bold md:gap-8">
+          <Link
+            href="/boards"
+            className={`hover:text-[--main] ${
+              pathName.includes("/boards") || pathName.includes("/addBoards")
+                ? "text-[--main]"
+                : ""
+            }`}
+          >
+            자유게시판
+          </Link>
+          <Link
+            href="/items"
+            className={`hover:text-[--main] ${
+              pathName === "/items" ? "text-[--main]" : ""
+            }`}
+          >
+            중고마켓
+          </Link>
+        </nav>
       </div>
-      <nav className="flex flex-grow-3 flex-row gap-2 text-lg font-bold md:gap-12">
-        <Link
-          href="/boards"
-          className={`hover:text-[--main] ${
-            pathName.includes("/boards") || pathName.includes("/addBoards")
-              ? "text-[--main]"
-              : ""
-          }`}
-        >
-          자유게시판
-        </Link>
-        <Link
-          href="/items"
-          className={`hover:text-[--main] ${
-            pathName === "/items" ? "text-[--main]" : ""
-          }`}
-        >
-          중고마켓
-        </Link>
-      </nav>
       {accessToken && (
         <Image src={icon_profile} alt="프로필" width={48} height={48} />
       )}
