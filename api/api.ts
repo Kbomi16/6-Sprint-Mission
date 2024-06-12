aa7c6a7dcd76aadf4bae29601cf6240ef927a7d5
김보미 <kimbomi2172@naver.com>
2024-06-12 16:00:54 +0900

    ♻️ refactor: axios URLSearchParams 추가

--- api/api.ts
+++ api/api.ts
@@ -3,9 +3,12 @@ import instance from "@/lib/axios";
 // 페이지네이션을 위한 전체 게시글 수
 export async function getTotalPosts() {
   try {
-    const { data } = await instance.get(
-      "/articles?&pageSize=10000&orderBy=recent",
-    );
+    const params = new URLSearchParams({
+      pageSize: "10000",
+      orderBy: "recent",
+    });
+
+    const { data } = await instance.get("/articles?", { params });
     return data.list.length;
   } catch (error) {
     console.error("getTotalPosts 함수에서 오류 발생:", error);
@@ -36,9 +39,11 @@ export async function getPosts({
 
 export async function getBestPosts({ pageSize = 3 }) {
   try {
-    const { data } = await instance.get(
-      `/articles?&pageSize=${pageSize}&orderBy=like`,
-    );
+    const params = new URLSearchParams({ orderBy: "like" });
+
+    const { data } = await instance.get(`/articles?&pageSize=${pageSize}`, {
+      params,
+    });
     return data.list;
   } catch (error) {
     console.error("getBestPosts 함수에서 오류 발생:", error);
@@ -58,9 +63,10 @@ export async function getPostsDetail(articleId: string) {
 
 export async function getPostsComments(articleId: string) {
   try {
-    const { data } = await instance.get(
-      `/articles/${articleId}/comments?limit=100`,
-    );
+    const params = new URLSearchParams({ limit: "100" });
+    const { data } = await instance.get(`/articles/${articleId}/comments?`, {
+      params,
+    });
     return data.list;
   } catch (error) {
     console.error("getPostsComments 함수에서 오류 발생:", error);
